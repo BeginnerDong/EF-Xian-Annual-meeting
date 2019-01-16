5639d46d04c7458123f77a6d5dddfb76e5b5515b
Mr zh <877854886@qq.com>
2019-01-16 09:53:19 +0800

    test

--- pages/cumpus/cumpus.js
+++ pages/cumpus/cumpus.js
@@ -5,18 +5,64 @@ const app = getApp();
 
 Page({
   data: {
-   
+   	mainData:[],
+   	isFirstLoadAllStandard:['getMainData']
   },
   //事件处理函数
  
   onLoad(options) {
     const self = this;
-
+    api.commonInit(self);
+    self.getMainData()
   },
+
+  getMainData(){
+    const  self =this;
+    const postData={};
+    postData.paginate = api.cloneForm(self.data.paginate);
+    postData.searchItem = {
+      thirdapp_id:getApp().globalData.solely_thirdapp_id
+    };
+    postData.getBefore ={
+     caseData:{
+        tableName:'label',
+        searchItem:{
+          title:['=',['校区']],
+        },
+        middleKey:'menu_id',
+        key:'id',
+        condition:'in',
+      },
+    };
+    postData.order = {
+    	listorder:'desc'
+    };
+    const callback =(res)=>{
+      if(res.info.data.length>0){
+        self.data.mainData.push.apply(self.data.mainData,res.info.data);
+      };
+      api.checkLoadAll(self.data.isFirstLoadAllStandard,'getMainData',self);
+      self.setData({
+        web_mainData:self.data.mainData,
+      });
+    };
+    api.articleGet(postData,callback);
+  },
+
+   onReachBottom() {
+    const self = this;
+    if(!self.data.isLoadAll&&self.data.buttonCanClick){
+      self.data.paginate.currentPage++;
+      self.getMainData();
+    };
+  },
+
   intoPath(e){
     const self = this;
     api.pathTo(api.getDataSet(e,'path'),'nav');
   },
+
+
   intoPathRedirect(e){
     const self = this;
     api.pathTo(api.getDataSet(e,'path'),'redi');
